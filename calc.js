780bdd430cfb2597ce0ed69b1b5df88d33926882
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-01-10 10:55:45 +0200

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,13 @@
+
+
+const summation = (a, b) =>  a + b;
+const subtraction = (a, b) =>  a - b;
+const multiply = (a, b) => a * b;
+const division = (a, b) =>  a / b;
+ 
+module.exports = {
+  summation,
+  subtraction,
+  multiply,
+  division
+}
