92fdf5b532ca9695e222aaa61969f7d2e18fe697
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-03-18 19:18:33 +0200

    Add files via upload

---
+++ express-practice/server.js
@@ -0,0 +1,23 @@
+const { response } = require("express");
+const path = require('path')
+const express = require("express");
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).send(path.join(__dirname, 'views', '404.html'))
+})
+
+
+
+app.listen(3000)
